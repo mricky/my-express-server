fccd2ccc744004ae07bbf3e1494b9c230454fbc6
Mohammad <mricky.it@gmail.com>
2019-12-02 21:33:50 +0700

    Sample Request and Response simple

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(request, response){
+	//response.send('hi ricky'); string
+	response.send('<h1>Hello World');
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
